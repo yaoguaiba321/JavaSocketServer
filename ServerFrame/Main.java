fd77eb35dc3940f09a18263980264495ab2b800a
yaoguaiba321 <65715187+yaoguaiba321@users.noreply.github.com>
2022-04-01 09:38:56 +0800

    Create Main.java

---
+++ ServerFrame/Main.java
@@ -0,0 +1,15 @@
+package Server;
+
+/**
+* 程序入口
+* @author 会编程的cpu
+* @version 1.0
+*/
+public class Main
+{
+	public static void main(String[] args)
+	{
+		AppServer  as = new AppServer();
+		as.execute();
+	}
+}
